482b26568a0afce3cf3fa099232d47af06ccac21
divyansh shukla <divyanshshukla3107@gmail.com>
2025-08-04 17:32:47 +0530

    added

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+// a=d
+// console.log("value of a is:", a)
+
+
+
+// redeclare and updation possible,global scope
+// var a = 10;
+// var a = 20;
+// console.log(a)
+
+
+// updation possible but not redeclaration,local scope
+
+// let a ; //declaration
+// a=5; //initialization
+// a=100;
+// let b =6; //dono kaam ek saath
+
+//updation and redeclaration not possible ,local schope
+// const adharid=9812738912371398174;
+// const adharid=012983123812381901213;
+
+
+// console.log(adharid)
+
+// let 1a=100;
+// let $ad=100;
+// let a =4;
+// let A=4;
+
+// let akashambani=100;
+// let akasha mbani=1---;
+
+
+
+
+
+
