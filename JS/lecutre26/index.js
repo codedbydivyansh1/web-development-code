8ec761310fb7bc57d2c36e8eeaa5b3633120609d
divyansh shukla <divyanshshukla3107@gmail.com>
2025-08-05 22:40:28 +0530

    done

---
+++ JS/lecutre26/index.js
@@ -0,0 +1,84 @@
+// let num =20;
+// console.log(typeof num)
+
+
+// let str="Divyansh"
+// console.log(typeof str)
+
+// let bool = false
+// console.log(typeof bool)
+// let b;
+// let a = null;
+// console.log(typeof a)
+// console.log(typeof b)
+
+
+// let  a =Symbol("divyanh")
+// console.log(typeof a)
+
+// let big = BigInt(1232930912301319319381)
+// console.log(big)
+
+// const ytvideo = {
+//     video : 'https://www.google.com',
+//     thumbnail:"https://www.youtube.com",
+
+//     tags:"lec26 ,js course,js basic to advance",
+//     adult:false
+
+
+// }
+// console.log(ytvideo)
+
+
+// Arithmetic operator
+
+
+// let a =4+7;
+// console.log(a)
+
+// let mul = 10*9;
+// console.log(mul)
+
+// const num1=5;
+// const num2=20;
+// let multi = num1/num2;
+// console.log(multi)
+
+// let div = 10%20
+// console.log(div)
+
+
+// let pow = 2**3;
+// console.log(pow)
+
+// let channel = 'codethread';
+// let val = 4;
+// val-=10
+// console.log(val)
+
+// let count = 4;
+
+// console.log(++count)
+
+
+//logical operator
+
+// console.log(2 && 0);
+// console.log(0 && 2);
+// console.log(0 || 2);
+// console.log(2 || 0);
+
+
+//comparison operators
+
+// let a =7.0>7;
+// let b=200==="200"
+// console.log(b)
+
+// let a ="divyansh"+" "+"shukla"
+// console.log(a)
+
+// let a =1+'1';
+// let b=1-'2';
+// console.log(a,b)
---
+++ JS/lecutre26/tempCodeRunnerFile.js
@@ -0,0 +1,46 @@
+// let num =20;
+// console.log(typeof num)
+
+
+// let str="Divyansh"
+// console.log(typeof str)
+
+// let bool = false
+// console.log(typeof bool)
+// let b;
+// let a = null;
+// console.log(typeof a)
+// console.log(typeof b)
+
+
+// let  a =Symbol("divyanh")
+// console.log(typeof a)
+
+// let big = BigInt(1232930912301319319381)
+// console.log(big)
+
+// const ytvideo = {
+//     video : 'https://www.google.com',
+//     thumbnail:"https://www.youtube.com",
+
+//     tags:"lec26 ,js course,js basic to advance",
+//     adult:false
+
+
+// }
+// console.log(ytvideo)
+
+
+// Arithmetic operator
+
+
+// let a =4+7;
+// console.log(a)
+
+let mul = 10*9;
+console.log(mul)
+
+const num1=5;
+const num2=20;
+let multi = num1/num2;
+console.log(multi)
